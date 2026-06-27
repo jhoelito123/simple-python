1652cc4b7e1d04bf5e868b1f5b6d1376dff5ce00
Adam Jhoel Mamani <133827076+jhoelito123@users.noreply.github.com>
2026-06-27 09:18:50 -0400

    Introduce DesactivateProductSerializer
    Added DesactivateProductSerializer for product status updates.

--- products/serializers.py
+++ products/serializers.py
@@ -7,3 +7,14 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Producto
         fields = "__all__"
+
+class DesactivateProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Producto
+        fields = ["status"]
+        read_only_fields = ["status"]
+
+    def update(self, instance, validated_data):
+        instance.status = False
+        instance.save()
+        return instance
